8c1b854c2f62be4a58c5e8e7b9d71b0fb66228af
Visar Domi <visardomi4@gmail.com>
2018-11-24 13:16:21 +0100

    Chapter 15: A better Application Structure

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -11,13 +14,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-
-    ###########
     ADMINS = ['lubu0900@gmail.com'] 
-    ###########
-
-    POSTS_PER_PAGE = 25
-
     LANGUAGES = ['en', 'es', 'sq']
-
     YANDEX_TRANSLATOR_KEY = os.environ.get('YANDEX_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
